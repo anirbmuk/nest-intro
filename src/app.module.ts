65ceb9c33fca7448731abcc07f9ad961e474452e
anirbmuk <anirbmuk@live.in>
2023-03-16 21:25:06 +0100

    chore: removed unused service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { ProductsModule } from './products/products.module';
-import { AppService } from './app.service';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
